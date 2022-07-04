aa0d08518bf2e489d255470aec4ca2a4648c41d6
RockyGitHub <chris.davidoff@gmail.com>
2022-07-04 16:33:06 -0600

    changes

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo pacman -S bspwm cava kitty picom polybar rofi sxhkd brillo pamixer betterlockscreen zsh pfetch
+yay -S nerd-fonts-jetbrains-mono icat
+
+
+
+#todo setup icat (alias for `kitty +kitten icat example.jpeg`)
+#todo add favorite zsh configuration
+#todo check if laptop battery gets displayed in polybar (use one of the statusbar scripts)
+#todo change the super key to modkey4?
