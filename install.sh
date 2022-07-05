b4c1567e1a144c5e0d5647f26bb7306d268601c2
Rocky_Felix <chris.davidoff@gmail.com>
2022-07-05 14:26:11 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,19 @@
 
 sudo pacman -S bspwm cava kitty picom polybar rofi sxhkd brillo pamixer betterlockscreen zsh pfetch
 yay -S nerd-fonts-jetbrains-mono icat
-
-
+curl -L http://install.ohmyz.sh | sh
+yay -S --noconfirm zsh-theme-powerlevel10k-git
+echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 #todo setup icat (alias for `kitty +kitten icat example.jpeg`)
 #todo add favorite zsh configuration
 #todo check if laptop battery gets displayed in polybar (use one of the statusbar scripts)
 #todo change the super key to modkey4?
+#todo install exa (ls replacement)
+#todo install delta (git diff replcaement)
+#todo install pp_json, zsh-autosuggestions (zsh plugins)
+#todo install neovim
+#todo install neovide
+#todo init on startup neovim host to work with neovide
+#todo create an app-image bin directory and add it to the PATH
+#todo update readme with new tools listed
