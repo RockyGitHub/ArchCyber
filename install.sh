39b6a3cc14fcd732927e156670ad862803245da7
Rocky_Felix <chris.davidoff@gmail.com>
2022-07-05 19:48:25 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
-sudo pacman -S bspwm cava kitty picom polybar rofi sxhkd brillo pamixer betterlockscreen zsh pfetch
-yay -S nerd-fonts-jetbrains-mono icat
+# Install packages
+sudo pacman -S bspwm cava kitty picom polybar rofi sxhkd brillo pamixer betterlockscreen zsh pfetch neovim neovide spotify
 curl -L http://install.ohmyz.sh | sh
-yay -S --noconfirm zsh-theme-powerlevel10k-git
+yay -S --noconfirm zsh-theme-powerlevel10k-git exa-git nerd-fonts-jetbrains-mono icat
 echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+# Install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rust.sh
+source ~/.cargo/env
+rustup self update
+
+# Make app-image bin directory
+mkdir /usr/$USER/app-images
+#todo add this to the .zshrc file      export PATH="usr/$USER/bin:$PATH"
 
 #todo setup icat (alias for `kitty +kitten icat example.jpeg`)
 #todo add favorite zsh configuration
 #todo check if laptop battery gets displayed in polybar (use one of the statusbar scripts)
 #todo change the super key to modkey4?
-#todo install exa (ls replacement)
 #todo install delta (git diff replcaement)
 #todo install pp_json, zsh-autosuggestions (zsh plugins)
-#todo install neovim
-#todo install neovide
 #todo create the config file for neovim to be copied in this install script
 #todo init on startup neovim host to work with neovide
-#todo create an app-image bin directory and add it to the PATH
 #todo create a ~/.p10k.zsh and upload it to this repo to be copied in this install script (enable rust_version!)
+
+#todo add aliases for:
+# exa
+# icat -- kitty +kitten icat
+
 #todo update readme with new tools listed
